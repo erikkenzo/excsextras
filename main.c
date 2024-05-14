9d5b3df9fae40fa93254dfc201607d2727b4d25e
erikkenzo <160777727+erikkenzo@users.noreply.github.com>
2024-05-14 19:54:32 +0000

    operações matriciais

--- main.c
+++ main.c
@@ -1,47 +1,115 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Definindo a estrutura Produto
-struct Produto {
-    char nome[50];
-    float preco;
-    int quantidade;
-};
+// Função para multiplicar duas matrizes
+int **multiplicarMatrizes(int **matriz1, int **matriz2, int linhas1, int colunas1, int colunas2) {
+    int **resultado;
+    resultado = (int **)malloc(linhas1 * sizeof(int *));
+    for (int i = 0; i < linhas1; i++) {
+        resultado[i] = (int *)malloc(colunas2 * sizeof(int));
+    }
 
-// Função para calcular o valor total em estoque de um produto
-float calcularValorTotal(struct Produto p) {
-    return p.preco * p.quantidade;
+    for (int i = 0; i < linhas1; i++) {
+        for (int j = 0; j < colunas2; j++) {
+            resultado[i][j] = 0;
+            for (int k = 0; k < colunas1; k++) {
+                resultado[i][j] += matriz1[i][k] * matriz2[k][j];
+            }
+        }
+    }
+    return resultado;
+}
+
+// Função para transpor uma matriz
+int **transporMatriz(int **matriz, int linhas, int colunas) {
+    int **transposta;
+    transposta = (int **)malloc(colunas * sizeof(int *));
+    for (int i = 0; i < colunas; i++) {
+        transposta[i] = (int *)malloc(linhas * sizeof(int));
+    }
+
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            transposta[j][i] = matriz[i][j];
+        }
+    }
+    return transposta;
+}
+
+// Função para liberar memória alocada para matriz
+void liberarMemoria(int **matriz, int linhas) {
+    for (int i = 0; i < linhas; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
+}
+
+// Função para imprimir uma matriz
+void imprimirMatriz(int **matriz, int linhas, int colunas) {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main() {
-    // Definindo um array de Produtos para armazenar múltiplos produtos
-    struct Produto produtos[50];
-    int numProdutos = 0;
-    char continuar;
+    int linhas1, colunas1, linhas2, colunas2;
 
-    do {
-        // Pedindo ao usuário para inserir os detalhes do produto
-        printf("Digite o nome do produto: ");
-        scanf("%s", produtos[numProdutos].nome);
+    printf("Informe o número de linhas e colunas da primeira matriz: ");
+    scanf("%d %d", &linhas1, &colunas1);
 
-        printf("Digite o preço do produto: ");
-        scanf("%f", &produtos[numProdutos].preco);
+    printf("Informe o número de linhas e colunas da segunda matriz: ");
+    scanf("%d %d", &linhas2, &colunas2);
 
-        printf("Digite a quantidade em estoque: ");
-        scanf("%d", &produtos[numProdutos].quantidade);
-
-        numProdutos++; // Incrementando o contador de produtos
-
-        // Perguntando se o usuário deseja continuar cadastrando produtos
-        printf("Deseja cadastrar outro produto? (S/N): ");
-        scanf(" %c", &continuar);
-    } while (continuar == 'S' || continuar == 's');
-
-    // Exibindo os produtos cadastrados e o valor total em estoque
-    printf("\nProdutos cadastrados:\n");
-    printf("Nome\t\tPreço\t\tQuantidade\tValor Total\n");
-    for (int i = 0; i < numProdutos; i++) {
-        printf("%s\t\t%.2f\t\t%d\t\t%.2f\n", produtos[i].nome, produtos[i].preco, produtos[i].quantidade, calcularValorTotal(produtos[i]));
+    if (colunas1 != linhas2) {
+        printf("As matrizes não podem ser multiplicadas. O número de colunas da primeira matriz deve ser igual ao número de linhas da segunda matriz.\n");
+        return 1;
     }
+
+    // Alocar memória para as matrizes
+    int **matriz1 = (int **)malloc(linhas1 * sizeof(int *));
+    for (int i = 0; i < linhas1; i++) {
+        matriz1[i] = (int *)malloc(colunas1 * sizeof(int));
+    }
+
+    int **matriz2 = (int **)malloc(linhas2 * sizeof(int *));
+    for (int i = 0; i < linhas2; i++) {
+        matriz2[i] = (int *)malloc(colunas2 * sizeof(int));
+    }
+
+    printf("Informe os elementos da primeira matriz:\n");
+    for (int i = 0; i < linhas1; i++) {
+        for (int j = 0; j < colunas1; j++) {
+            scanf("%d", &matriz1[i][j]);
+        }
+    }
+
+    printf("Informe os elementos da segunda matriz:\n");
+    for (int i = 0; i < linhas2; i++) {
+        for (int j = 0; j < colunas2; j++) {
+            scanf("%d", &matriz2[i][j]);
+        }
+    }
+
+    // Multiplicar as matrizes
+    int **resultadoMultiplicacao = multiplicarMatrizes(matriz1, matriz2, linhas1, colunas1, colunas2);
+
+    printf("\nResultado da multiplicação:\n");
+    imprimirMatriz(resultadoMultiplicacao, linhas1, colunas2);
+
+    // Transpor a primeira matriz
+    int **transpostaMatriz1 = transporMatriz(matriz1, linhas1, colunas1);
+
+    printf("\nTransposta da primeira matriz:\n");
+    imprimirMatriz(transpostaMatriz1, colunas1, linhas1);
+
+    // Liberar memória alocada para as matrizes
+    liberarMemoria(matriz1, linhas1);
+    liberarMemoria(matriz2, linhas2);
+    liberarMemoria(resultadoMultiplicacao, linhas1);
+    liberarMemoria(transpostaMatriz1, colunas1);
 
     return 0;
 }
